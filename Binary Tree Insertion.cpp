ea0b367623a329fa9808383089bad068c75bd957
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:31:07 +0530

    Update Binary Tree Insertion.cpp

--- Binary Tree Insertion.cpp
+++ Binary Tree Insertion.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 using namespace std;
-class node
-{
+class node{
 	public:
 	int data;
 	node *left,*right;
 };
-node* creat()
-{
+node* creat(){
 	int x;
 	cout<<"\nEnter Data(-1 for no node)  ";cin>>x;
 	if(x==-1)
@@ -18,12 +16,9 @@ node* creat()
 	newnode->left=creat();
 	cout<<"\nEnter right child of "<<x;
 	newnode->right=creat();
-	
 	return newnode;
 }
-int main()
-{
+int main(){
 	node *root=0;
-	
 	root=creat();
 }
