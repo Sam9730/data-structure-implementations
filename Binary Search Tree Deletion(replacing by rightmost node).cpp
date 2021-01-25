03ab2dc6ea42fe9f3ee7780da3b7239501136c28
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:25:07 +0530

    Update Binary Search Tree Deletion(replacing by rightmost node).cpp

--- Binary Search Tree Deletion(replacing by rightmost node).cpp
+++ Binary Search Tree Deletion(replacing by rightmost node).cpp
@@ -5,9 +5,7 @@ class node{
 	int data;
 	node *left,*right;
 };
-
-node* insert(node *root,int data)
-{
+node* insert(node *root,int data){
 	if(root==0)
 	{
 		node *newnode=new node();
@@ -17,45 +15,33 @@ node* insert(node *root,int data)
 		return newnode;
 	}
 	else if(root->data<data)
-	{
 		root->right=insert(root->right,data);
-	}
 	else
-	{
 		root->left=insert(root->left,data);
-	}
-		return root;
+	return root;
 }
-
-void preorder(node* root)
-{
+void preorder(node* root){
 	if(root==0)
 		return ;
 	cout<<" "<<root->data;
-		preorder(root->left);
-		preorder(root->right);
+	preorder(root->left);
+	preorder(root->right);
 }
-
-void inorder(node* root)
-{
+void inorder(node* root){
 	if(root==0)
 		return ;
 	inorder(root->left);
 	cout<<" "<<root->data;
 	inorder(root->right);
 }
-
-void postorder(node* root)
-{
+void postorder(node* root){
 	if(root==0)
 		return ;
 	postorder(root->left);
 	postorder(root->right);
 	cout<<" "<<root->data;
 }
-
-node* search(node* root,int data)
-{
+node* search(node* root,int data){
 	while(root->data!=data && root!=0)
 	{
 		if(root->data>data)
@@ -65,22 +51,18 @@ node* search(node* root,int data)
 	}
 	if(root->data==data)
 		return root;
-	
 }
-void deleteNode(node *root,int data)
-{
+void deleteNode(node *root,int data){
 	node *parent,*temp=search(root,data);
 	while(root->right!=0)
 	{
 		parent=root;
 		root=root->right;
 	}
-	
 	temp->data=root->data;
 	parent->right=0;
 	cout<<"\n"<<root->data<<" Deleted"; 
 	delete root;
-	
 }
 int main() {
 	node *root=0;
@@ -104,6 +86,5 @@ int main() {
 	inorder(root);
 	cout<<"\nPostorder Traversal After Delete: ";
 	postorder(root);
-	
 	return 0;
 }
