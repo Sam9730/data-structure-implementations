60003d1cbf219579178992b566f8056c7f0d876d
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:48:21 +0530

    Update HASH TABLE Insert,Delete,Search.cpp

--- HASH TABLE Insert,Delete,Search.cpp
+++ HASH TABLE Insert,Delete,Search.cpp
@@ -1,20 +1,15 @@
 #include<iostream>
 using namespace std;
 int size,*hashArray;
-int hashIndex(int key)
-{
+int hashIndex(int key){
 	return key%size;
 }
-void insert()
-{
+void insert(){
 	int data,index;
 	cout<<"\nENter data to insert in Hash table ";cin>>data;
 	index=hashIndex(data);
-	
 	while(hashArray[index]!=0 && index<size)
-	{
 		index=(index+1)%size;
-	}
 	if(index==size)
 		cout<<"\nHASH TABLE is Full !!! ";
 	else
@@ -22,35 +17,24 @@ void insert()
 		hashArray[index]=data;
 		cout<<"\n"<<hashArray[index]<<" added to HASH TABLE ";
 	}
-		
 }
-
-void search()
-{
+void search(){
 	int data,index;
 	cout<<"\nEnter data to be searched Hash table ";cin>>data;
-	index=hashIndex(data);
-	
+	index=hashIndex(data);	
 	while(hashArray[index]!=data && index<size)
-	{
 		index=(index+1)%size;
-	}
 	if(index==size)
 		cout<<"\nData is not presentin HASH TABLE";
 	else
 		cout<<"\n"<<hashArray[index]<<" is present at "<<index<<"th Index of Hash function.";
 }
-
-void delet()
-{
+void delet(){
 	int data,index;
 	cout<<"\nENter data to deleted from Hash table ";cin>>data;
-	index=hashIndex(data);
-	
+	index=hashIndex(data);	
 	while(hashArray[index]!=data && index<size)
-	{
 		index=(index+1)%size;
-	}
 	if(index==size)
 		cout<<"\nData is not presentin HASH TABLE";
 	else
@@ -58,28 +42,21 @@ void delet()
 		cout<<"\n"<<hashArray[index]<<" deleted ";
 		hashArray[index]=0;
 	}
-		
 }
-
-void display()
-{
+void display(){
 	int i;
 	cout<<"\nHASH TABLE : \n";
 	cout<<"\nIndex\tValue\n";
 	for(i=0;i<size;i++)
-	{
 		cout<<i<<"\t\t"<<hashArray[i]<<"\n";
-	}
 }
-int main()
-{
+int main(){
 	int choice;
 	cout<<"\nEnter size of array ";cin>>size;
 	hashArray=new int(size);
 	do{
 	cout<<"\n~~~	MENU	~~~\n1-> Insert\t2-> Delete\t3-> Display\t4-> Searching\t5-> Exit\nEnter your choice  ";
 		cin>>choice;
- 
 		switch(choice){
 			case 1:insert();break;
 			case 2:delet();break;
@@ -88,6 +65,5 @@ int main()
 			default:cout<<"Incorrect Choice !!! Please Re-Enter correct choice\n";break;
 		}
 	}while(choice!=5);
- 
 	return 0;
 }
