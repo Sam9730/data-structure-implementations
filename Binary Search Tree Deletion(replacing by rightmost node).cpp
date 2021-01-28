60202b6cb3f93c79918eddd84fee64bc04fa6211
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 14:03:46 +0530

    code description added

--- Binary Search Tree Deletion(replacing by rightmost node).cpp
+++ Binary Search Tree Deletion(replacing by rightmost node).cpp
@@ -5,6 +5,9 @@ class node{
 	int data;
 	node *left,*right;
 };
+/* insert() func. will create nodes for left child of root by recursively calling itself untill user enter -1,then it will create nodes for right
+  child untill user again enters -1.
+  func will return node pointers of each subroot */
 node* insert(node *root,int data){
 	if(root==0)
 	{
@@ -20,7 +23,7 @@ node* insert(node *root,int data){
 		root->left=insert(root->left,data);
 	return root;
 }
-void preorder(node* root){
+void preorder(node* root){	//prints in (Root, Left, Right)
 	if(root==0){
 		return ;
 	}
@@ -28,7 +31,7 @@ void preorder(node* root){
 	preorder(root->left);
 	preorder(root->right);
 }
-void inorder(node* root){
+void inorder(node* root){	//prints in (Left, Root, Right)
 	if(root==0){
 		return ;
 	}
@@ -36,7 +39,7 @@ void inorder(node* root){
 	cout<<" "<<root->data;
 	inorder(root->right);
 }
-void postorder(node* root){
+void postorder(node* root){	//prints in (Left, Right, Root)
 	if(root==0){
 		return ;
 	}
@@ -44,7 +47,7 @@ void postorder(node* root){
 	postorder(root->right);
 	cout<<" "<<root->data;
 }
-node* search(node* root,int data){
+node* search(node* root,int data){	//search for a node in a tree and return its position if found else return 0.
 	while(root->data!=data && root!=0)
 	{
 		if(root->data>data)
@@ -55,15 +58,15 @@ node* search(node* root,int data){
 	if(root->data==data)
 		return root;
 }
-void deleteNode(node *root,int data){
-	node *parent,*temp=search(root,data);
+void deleteNode(node *root,int data){	//replace the node with its rightmost child of the tree.
+	node *parent,*temp=search(root,data);//search function will return data's location which will be stored in temp pointer.
 	while(root->right!=0)
 	{
-		parent=root;
+		parent=root;	//parent pointr will always point to parent node of root.
 		root=root->right;
-	}
-	temp->data=root->data;
-	parent->right=0;
+	}//root will be now pointing to rightmost child of tree.
+	temp->data=root->data;	//element replaced by rightmost child
+	parent->right=0;	//rightmost root deleted
 	cout<<"\n"<<root->data<<" Deleted"; 
 	delete root;
 }
