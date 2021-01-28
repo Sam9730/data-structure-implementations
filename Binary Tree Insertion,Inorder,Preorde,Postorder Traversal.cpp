fb76ae0d410c474848a9b4911c4e2a6d8de832a9
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 13:11:40 +0530

    code description added

--- Binary Tree Insertion,Inorder,Preorde,Postorder Traversal.cpp
+++ Binary Tree Insertion,Inorder,Preorde,Postorder Traversal.cpp
@@ -5,27 +5,30 @@ class node{
 	int data;
 	node *left,*right;
 };
-void inorder(node *root){
+void inorder(node *root){	//prints in (Left, Root, Right)
 	if(root==0)
 		return ;
 	inorder(root->left);
 	cout<<" "<<root->data;
 	inorder(root->right);
 }
-void preorder(node *root){
+void preorder(node *root){`	//prints in (Root, Left, Right)
 	if(root==0)
 		return ;
 	cout<<" "<<root->data;
 	preorder(root->left);
 	preorder(root->right);
 }
-void postorder(node *root){
+void postorder(node *root){	//prints in (Left, Right, Root)
 	if(root==0)
 		return ;
 	postorder(root->left);
 	postorder(root->right);
 	cout<<" "<<root->data;
 }
+/* creat() func. will create nodes for left child of root by recursively calling itself untill user enter -1,then it will create nodes for right
+  child untill user again enters -1.
+  func will return node pointers of each subroot */
 node* creat(){
 	int x;
 	cout<<"\nEnter Data(-1 for no node)  ";cin>>x;
@@ -41,8 +44,8 @@ node* creat(){
 	return newnode;
 }
 int main(){
-	node *root=0;
-	root=creat();
+	node *root=0;	//created root pointer for tree	
+	root=creat();	//create will return root value of tree which will be assign to root pointer
 	cout<<"\nInorder Traversal : ";
 	inorder(root);
 	cout<<"\nPreorder Traversal : ";
