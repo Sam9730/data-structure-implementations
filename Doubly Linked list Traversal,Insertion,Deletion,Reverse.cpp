b2c0b4ed762b2613179bca69ec80ec7db14fae65
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 11:50:59 +0530

    code description

--- Doubly Linked list Traversal,Insertion,Deletion,Reverse.cpp
+++ Doubly Linked list Traversal,Insertion,Deletion,Reverse.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 using namespace std;
-class node{
+class node{	//will be used as node of a linked list
 	public:
 	int data;
 	node *pre;
 	node *next;
 };
-void push(node** head){
-	node *newnode=new node();
+//insert data @ start of link list.
+void push(node** head){		//recieve address of head pointer using double pointer reference name head.
+	node *newnode=new node();	//create a node name newnode
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	newnode->pre=0;
 	newnode->next=*head;
 	*head=newnode;
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
+//insert node @ specific position
 void insertAt(node** head){
-	int pos,c=1;
+	int pos,c=1;	//c-count the no. of node passed to reach the required position in linked list
 	node *newnode=new node();
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	newnode->pre=0;
 	newnode->next=0;
 	cout<<"\nEnter position at which you want to insert data\t";cin>>pos;
-	if(*head==0)
+	if(*head==0)	//if LL is empty
 		*head=newnode;
 	else
 	{
@@ -31,15 +33,15 @@ void insertAt(node** head){
 			prev=temp;
 			temp=temp->next;
 			c++;
-		}
-		newnode->pre=prev;
-		newnode->next=temp;
-		prev->next=newnode;
-		temp->pre=newnode;
+		}	//temp will reach to desired position(pos) and prev will point to its previous node
+		newnode->pre=prev;	//newnode's pre will point to previous node
+		newnode->next=temp;	//newnode's next will point to node presentg at the desired position(pos)
+		prev->next=newnode;	//previous node's next will point to newnode i.e newnode will be inserted @ desired position
+		temp->pre=newnode;	
 	}
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
-void append(node** head){
+void append(node** head){	//insert node @ end
 	node *newnode=new node();
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	newnode->pre=0;
@@ -49,27 +51,29 @@ void append(node** head){
 	else
 	{
 		node *temp=*head;
-		while(temp->next!=0)
+		while(temp->next!=0){
 			temp=temp->next;
+		}	//temp will point to end node
 		newnode->pre=temp;
-		temp->next=newnode;
+		temp->next=newnode;	//last node's next pointer will point newnode i.e newnode will become last node
 	}
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
-void deleteNode(node** head){
+void deleteNode(node** head){	//take key data from user & search if it is present in LL,if found then delete the corresponding node.
 	node *temp=*head;
 	int key;
 	cout<<"\nEnter Data to delete \t";cin>>key;
-	while(temp!=0 && temp->data!=key)
+	while(temp!=0 && temp->data!=key){	//using temp ,key data will be searched in whole LL(Linked List).
 		temp=temp->next;
-	if(temp==0)
+	}
+	if(temp==0)	//if temp reaches end and key data isn't found
 		cout<<"\n !!! ~~~ Data not found ~~~ !!!";
-	else
+	else	//i.e temp is pointing key data node
 	{
-		node *frwd=temp->next,*bkwd=temp->pre;
-		frwd->pre=bkwd;
-		bkwd->next=frwd;
-		delete temp;
+		node *frwd=temp->next,*bkwd=temp->pre;//frwd-point to next node temp,bkwrd-point to previous node of temp
+		frwd->pre=bkwd;		//next node will be pointing to pre node.
+		bkwd->next=frwd;	//previous node's will be pointing to next node
+		delete temp;	//the node having key data & that temp pointer is pointing will be deleted
 		cout<<"\n Node Deleted ";
 	}
 }
@@ -77,12 +81,13 @@ void reverse(node** head){
 	node*temp=*head,*t;
 	while(temp!=0)
 	{
+		//Every node's pre & next pointer will swap each others value
 		t=temp->pre;
 		temp->pre=temp->next;
 		temp->next=t;
 		temp=temp->pre;
 	}
-	*head=t->pre;
+	*head=t->pre;	//head will point to last node & LL will start from last i.e Reversed
 }
 void display(node** head){
 	node *temp=*head;
