74cfec1e98164aa6a38e6d88044fc79983b1de32
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 13:21:56 +0530

    code description added

--- Binary Search Tree Insertion,All Order Traversal.cpp
+++ Binary Search Tree Insertion,All Order Traversal.cpp
@@ -5,8 +5,11 @@ class node{
 	int data;
 	node *left,*right;
 };
+/*func. will compare inserting value with root value and as per result it will recursively call its self for left subtree(if data is small) or for right subtree(if data is greater)
+,func. will be called untill root reaches 0 i.e no element left to traverse and then it will create nodes and return its adress corrresponding root of that value.
+  func will return node pointers of each subroot */
 node* insert(node *root,int data){
-	if(root==0)
+	if(root==0)//if root=0 i.e no next element remain in tree then it will insert newnode at that root pointer.
 	{
 		node *newnode=new node();
 		newnode->data=data;
@@ -14,13 +17,13 @@ node* insert(node *root,int data){
 		newnode->right=0;
 		return newnode;
 	}
-	else if(root->data<data)
-		root->right=insert(root->right,data);
-	else
-		root->left=insert(root->left,data);
+	else if(root->data<data)	//if inserting data is greater than root data then it will be added in right child of root	 
+		root->right=insert(root->right,data);//recursively call insert untill no child is left of right side i.e root=0
+	else				//if inserting data is less than root data then it will be added in left child of root
+		root->left=insert(root->left,data);//recursively call insert untill no child is left of left side i.e root=0
 	return root;
 }
-void preorder(node* root){
+void preorder(node* root){	//prints in (Root, Left, Righ)
 	if(root==0){
 		return ;
 	}
@@ -28,7 +31,7 @@ void preorder(node* root){
 	preorder(root->left);
 	preorder(root->right);
 }
-void inorder(node* root){
+void inorder(node* root){	//prints in (Left, Root, Right)
 	if(root==0){
 		return ;
 	}
@@ -36,7 +39,7 @@ void inorder(node* root){
 	cout<<" "<<root->data;
 	inorder(root->right);
 }
-void postorder(node* root){
+void postorder(node* root){	//prints in (Left, Right, Root)
 	if(root==0){
 		return ;
 	}
