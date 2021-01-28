be67058a46d6a3d18a254edede86a726b5c65eff
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 14:16:23 +0530

    code description added

--- GRAPH ---DFS using adjency matrix.cpp
+++ GRAPH ---DFS using adjency matrix.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-int adj[20][20],n,m,top=-1,k,j,i;
-int *stack=new int(n);			 
-int *visited=new int(n+1);
-int dfs(int t){
-	cout<<"->"<<t;
-	visited[t]=1;
-	stack[++top]=t;
-	for(int l;l<=n;l++)
+int adj[20][20],n,m,top=-1,k,j,i;	//adj-adjency matrix,n-no. of vertex,m-no. of columns,top-points top element of stack
+int *stack=new int(n);			 //stack to store vertex 
+int *visited=new int(n+1);		//visited to mark visited vertex
+int dfs(int t){		//takevertex in t parameter
+	cout<<"->"<<t;	
+	visited[t]=1;	//t index is marked 1 in visited array
+	stack[++top]=t;	//t added in stack
+	for(int l;l<=n;l++)	//checks for edges from t to other elements
 	{
-		if(adj[t][l]==1 && visited[l]!=1)
+		if(adj[t][l]==1 && visited[l]!=1)	//if edge is founf in t,l and l isnt visited yet,then recursivelly call dfs passing l as vertex.
 			dfs(l);
 	}
-	top--;
+	top--;	//top element of stack deleted
 	return 0;
 }
 int main(){
