880cb7e0762ea80bca77c8ca336dae48d8c0f49e
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 11:30:48 +0530

    code description added

--- Singly Linked list Traversal,Insertion,Deletion,Reverse.cpp
+++ Singly Linked list Traversal,Insertion,Deletion,Reverse.cpp
@@ -1,72 +1,76 @@
 #include <iostream>
 using namespace std;
-class node{
+class node{	//will be used as node of a linked list
 	public:
 	int data;
 	node *next;
 };
-void push(node** head){
-	node *newnode=new node();
+//insert data @ start of link list.
+void push(node** head){	//recieve address of head pointer using double pointer reference name head.
+	node *newnode=new node();	//create a node name newnode
 	cout<<"\nEnter Data\t";cin>>newnode->data;
 	newnode->next=*head;
-	*head=newnode;
+	*head=newnode;	`		//head pointer will point to newnode i.e newnode will be added @ start
 	cout<<"\nLink added at start \n";
 }
-void insertAfter(node** head){
-	int i,count=1;
+void insertAfter(node** head){	//insert node after a specific position
+	int i,count=1; //count-count the no. of node present in linked list
 	node *temp=*head;;
 	cout<<"\nEnter position after which you wantto insert data";cin>>i;
-	if(i<0)
+	if(i<0){
 		cout<<"\nIncorrect Position plz re-enter position";cin>>i;
-	node *newnode=new node();
-	cout<<"\nEnter Data \t";cin>>newnode->data;
-	while(count!=i)
-	{
-		temp=temp->next;
-		count++ ;
 	}
-	newnode->next=temp->next;
-	temp->next=newnode;
-	cout<<"\nNode added \n";
+	else{
+		node *newnode=new node();
+		cout<<"\nEnter Data \t";cin>>newnode->data;
+		while(count!=i)
+		{
+			temp=temp->next;
+			count++ ;
+		}//temp will reach to ith position(after which we need to add newnode)
+		newnode->next=temp->next;
+		temp->next=newnode;
+		cout<<"\nNode added \n";
+	}
 }
-void append(node** head){
+void append(node** head){	//insert node @ end
 	node *temp=*head;
 	node *newnode=new node();
 	cout<<"\nEnter Data\t";cin>>newnode->data;
 	while(temp->next!=0){
 		temp=temp->next;
-	}
+	}			//temp will reach to end node
 	newnode->next=0;
-	temp->next=newnode;
+	temp->next=newnode;	//last node's next pointer will point newnode i.e newnode will become last node
 	cout<<"\nNode added \n";
 }
-void deleteNode(node** head){
+void deleteNode(node** head){//take key data from user & search if it is present in LL,if found then delete the corresponding node.
 	int key;
 	cout<<"\nEnter data key to delete \t";
 	cin>>key;
 	node *temp=*head;
 	node *pre;
-	while(temp!=0 && temp->data!=key)
+	while(temp!=0 && temp->data!=key)//using temp ,key data will be searched in whole LL(Linked List).
 	{
 		pre=temp;
 		temp=temp->next;
 	}
-	if(temp==0)
+	if(temp==0)	//if temp reaches end and key data isn't found
 		return ;
 	pre->next=temp->next;
-	delete temp;
+	delete temp;	//the the node that temp pointer is pointing
 	cout<<"\nNode DELETED";
 }
 void reverse(node** head){
-	node *crnt=*head,*post=*head,*pre=0;
+	node *crnt=*head,*post=*head,*pre=0; //pre-point to previous node,crnt-point to current node,post-will poin t to next node
 	while(post!=0)
 	{
 		post=post->next;
-		crnt->next=pre;
-		pre=crnt;
+		crnt->next=pre;	
+		pre=crnt;		
 		crnt=post;
 	}
-	*head=pre;
+	*head=pre;	//head will point to last node & LL will start from last i.e Reversed 
 }
 void display(node** head){
 	node *temp=*head;
@@ -89,7 +93,7 @@ int main(){
 			case 1:cout<<"\n1.Insert at start\t2.Insrt at position\t3.Insert at end\nEnter Choice \t";cin>>i;
 				switch(i)
 				{
-					case 1:push(&head);break;
+					case 1:push(&head);break;	
 					case 2:insertAfter(&head);break;
 					case 3:append(&head);break;
 					default:break;
