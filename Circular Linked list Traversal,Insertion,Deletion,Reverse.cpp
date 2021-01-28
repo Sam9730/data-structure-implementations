8cbc792007a3747c01b3adec61a595811ffd03da
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 12:14:50 +0530

    code description added

--- Circular Linked list Traversal,Insertion,Deletion,Reverse.cpp
+++ Circular Linked list Traversal,Insertion,Deletion,Reverse.cpp
@@ -1,12 +1,14 @@
+//In circular Linked List the last node will point towards the first node(heaad node)
 #include <iostream>
 using namespace std;
-class node{
+class node{	//will be used as node of a linked list
 	public:
 	int data;
 	node *next;
 };
-void push(node** head){
-	node *newnode=new node();
+//insert data @ start of link list.
+void push(node** head){			//insert data @ start of link list.
+	node *newnode=new node();	//create a node name newnode
 	node *temp=*head;
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	if(*head==0)
@@ -18,15 +20,15 @@ void push(node** head){
 	{
 		while(temp->next!=*head){
 			temp=temp->next;
-		}
-		temp->next=newnode;
-		newnode->next=*head;
-		*head=newnode;
+		}	//temp will reach to last node
+		temp->next=newnode;	//last node will point to newnode
+		newnode->next=*head;	//newnode point to head node
+		*head=newnode;		//head node will point to newnode
 	}
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
-void insertAt(node** head){
-	int pos,c=1;
+void insertAt(node** head){	//insert node @ specific position
+	int pos,c=1;		//c-count the no. of node passed to reach the required position in linked list
 	node *newnode=new node();
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	cout<<"\nEnter position at which you want to insert data\t";cin>>pos;
@@ -40,51 +42,52 @@ void insertAt(node** head){
 			prev=temp;
 			temp=temp->next;
 			c++;
-		}
+		}//temp will reach to ith position(@ which we need to add newnode) and prev will point to previous node
 		newnode->next=temp;
 		prev->next=newnode;
 	}
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
-void append(node** head){
+void append(node** head){	//insert node @ end
 	node *newnode=new node();
 	cout<<"\nEnter data \t";cin>>newnode->data;
 	newnode->next=0;
 	node *temp=*head;
 	while(temp->next!=*head){
 		temp=temp->next;
-	}
-	temp->next=newnode;
-	newnode->next=*head;	
+	}	//temp will point to end node
+	temp->next=newnode;	//last node will point to newnode
+	newnode->next=*head;	//newnode will point to head node i.e newnode will be inserted at end
 	cout<<"\n"<<newnode->data<<" added to link list\n";
 }
-void deleteNode(node** head){
+void deleteNode(node** head){	//take key data from user & search if it is present in LL,if found then delete the corresponding node.
 	node *temp=*head,*pre=*head;
 	int key;
 	cout<<"\nEnter Data to delete \t";cin>>key;
-	while(temp->next!=*head && temp->data!=key)
+	while(temp->next!=*head && temp->data!=key)	//using temp ,key data will be searched in whole LL(Linked List).
 	{
 		pre=temp;
 		temp=temp->next;
-	}
-	if(temp->next==*head && temp->data!=key)
+	}	//pre will point to previous node of temp
+	if(temp->next==*head && temp->data!=key)	//node not found
 		cout<<"\n !!! ~~~ Data not found ~~~ !!!";
-	else if(temp->next==*head && temp->data==key)
+	else if(temp->next==*head && temp->data==key)	//node found @ end
 	{
 		pre->next=*head;
 		delete temp;
 		cout<<"\n!!! ~~~ Data deleted ~~~ !!!";
 	}
-	else if(temp==*head && temp->data==key)
+	else if(temp==*head && temp->data==key)		//node found @ start i.e head
 	{
-		while(temp->next!=*head)
+		while(temp->next!=*head){
 			temp=temp->next;
-		temp->next=pre->next;
+		}//temp will be pointing last node & pre will be pointing head
+		temp->next=pre->next;	
 		*head=pre->next;
 		delete pre;
 		cout<<"\n!!! ~~~ Data deleted ~~~ !!!";
 	}
-	else if(temp!=*head)
+	else if(temp!=*head)				//node found at temp position
 	{
 		pre->next=temp->next;
 		delete temp;
@@ -104,9 +107,9 @@ void display(node** head){
 int main() {
 	int choice,i;
 	node *head=0;
-	while(choice!=5)
+	do
 	{
-		cout<<"\n\n\n1.Insert\t2.Delete\t4.Display\t5.Exit\nEnter your Choice \t";
+		cout<<"\n\n\n1.Insert\t2.Delete\t3.Display\t4.Exit\nEnter your Choice \t";
 		cin>>choice;
 		switch(choice)
 		{
@@ -119,8 +122,8 @@ int main() {
 					default:break;
 				};break;
 			case 2:deleteNode(&head);break;
-			case 4:display(&head);break;
-			case 5:cout<<"\n~~~~ THANK YOU ~~~~";break;
+			case 3:display(&head);break;
+			case 4:cout<<"\n~~~~ THANK YOU ~~~~";break;
 		}
-	}
+	}while(choice!=4);
 }
