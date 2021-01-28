2e6be2014e7a67e5fcacd42bdb2309951a6a020d
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-28 14:10:14 +0530

    code description added

--- GRAPH ---BFS using adjency matrix.cpp
+++ GRAPH ---BFS using adjency matrix.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int adj[10][10],n,m,v;
+	int adj[10][10],n,m,v;	//adj-adjency matrix,n-no. of vertex,m-no. of edges.v-stores current vertex
 	cout<<"\nEnter no. of Vertex ";cin>>n;
 	cout<<"\nEnter no. of Edges ";cin>>m;
-	int *q=new int(n);
+	int *q=new int(n);	//Queue of size n 
 	int front=0,rear=-1;	//Queue Variables 
-	int *visited=new int(n);
+	int *visited=new int(n);//visited array to store visited indexes
 	int k,j,i;				//Loop variables
 	cout<<"\nEnter Edges : ";
 	for(k=1;k<=m;k++)
@@ -15,22 +15,22 @@ int main(){
 		adj[i][j]=1;
 	}
 	cout<<"\nEnter Starting Vertex : ";cin>>v;
-	q[++rear]=v;
-	visited[v]=1;
+	q[++rear]=v;	//starting vertex will be added in queue
+	visited[v]=1;	//v index of visited array will be marked 1
 	cout<<"\nBreadth First Traversal : ";
 	k=1;
 	while(k<=n)
 	{
 		for(i=0;i<=n;i++)
 		{
-			if(adj[v][i]==1 && visited[i]==0)
+			if(adj[v][i]==1 && visited[i]==0) //if there is a edge from vertex v to i, and if i is not visited then i is added in q and marked visited.
 			{
 				q[++rear]=i;
 				visited[i]=1;
 			}
 		}
-		cout<<v<<" ";
-		v=q[++front];
+		cout<<v<<" ";	//vertex is printed
+		v=q[++front];	//printed vertex is removed from Queue and front points to next element,v is assign next elemnt of Queue
 		k++;
 	}
 	return 0;
